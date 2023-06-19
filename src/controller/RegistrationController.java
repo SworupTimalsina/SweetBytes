b621717769de980a1894cb80320a50fb757dd34f
unknown <abajracharya55@gmail.com>
2023-06-19 13:35:08 +0545

    login ui

--- src/controller/RegistrationController.java
+++ src/controller/RegistrationController.java
@@ -36,6 +36,7 @@ public class RegistrationController {
                 String checkCredsResult=checkCreds.CheckRegistrationPageRegulation();
                 if(checkCredsResult.equals("ok")){
                     System.out.println("else statement of actionPerformed");
+                    view.displayMessage("user registered");
                     InsertRegistrationData();
                 }
                 else{
@@ -52,7 +53,7 @@ public class RegistrationController {
                 System.out.println("Try statement of InsertRegistration");
                 Statement stmt=conn.createStatement();
                 //String DOB=yearField.getSelectedItem().toString()+"-"+monthField.getSelectedItem().toString()+"-"+dayField.getSelectedItem().toString();
-                String sql = "insert into Creds(f_name, l_name, email, DOB, u_name, pass) values('"+model.getFirstName()+"','"+model.getLastName()+"','"+model.getEmail()+"','"+model.getDateOfBirth()+"','"+model.getUsername()+"','"+model.getPassword()+"')";
+                String sql = "insert into creds(f_name, l_name, email, DOB, u_name, pass, securityQ, answer) values('"+model.getFirstName()+"','"+model.getLastName()+"','"+model.getEmail()+"','"+model.getDateOfBirth()+"','"+model.getUsername()+"','"+model.getPassword()+"','"+model.getSecurity()+"','"+model.getAnswer()+"')";
             stmt.executeUpdate(sql);
             return true;
             }
---
+++ src/sweetbytes/SweetBytes.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sweetbytes;
+
+import view.LoginView;
+
+/**
+ *
+ * @author AduMan
+ */
+public class SweetBytes {
+    public static void main(String[] args){
+        LoginView LG = new LoginView();
+        LG.show();
+    }
+    
+}

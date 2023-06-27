4c3072c7fc08b3a0f58e7ea57568885ef4addb58
Allan Gautam <allan.gtm92@gmail.com>
2023-06-27 20:04:44 +0545

    added inheretance

--- src/model/LoginModel.java
+++ src/model/LoginModel.java
@@ -1,29 +1,12 @@
 package model;
 
 
-public class LoginModel {
-String username,password;  
+public class LoginModel extends RegistrationModel{
+  
 
 
-public LoginModel(String username,String password)
-{
-    this.username=username;
-    this.password=password;
-}
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    public LoginModel(String username,String password){
+        this.username=username;
+        this.password=password;
     }
 }

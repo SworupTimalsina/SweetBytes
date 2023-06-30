2935322f82a9f6e080d2cb8aa2dd32fdfdee7735
Allan Gautam <allan.gtm92@gmail.com>
2023-06-30 18:56:23 +0545

    minor bug fix

--- src/controller/CustomizeCakeController.java
+++ src/controller/CustomizeCakeController.java
@@ -1,15 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package controller;
 
-import view.*;
-import model.*;
-
-
+/**
+ *
+ * @author Allan
+ */
 public class CustomizeCakeController {
-	CustomizeCakeView cView;
 	
-	public CustomizeCakeController(CustomizeCakeView cView){
-		this.cView=cView;
-		
-	}
-		
 }

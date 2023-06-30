2b5c4abc42f393885ba059e0d2af63d73d073dcb
Allan Gautam <allan.gtm92@gmail.com>
2023-06-30 16:59:41 +0545

    added classes

--- src/Regulations/ProfilePageRegulation.java
+++ src/Regulations/ProfilePageRegulation.java
@@ -1,18 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Regulations;
 
 import model.RegistrationModel;
 
-/**
- *
- * @author Allan
- */
 public class ProfilePageRegulation extends RegistrationPageRegulation{
-    
-    
     /*
     * A class that validates the user given information.
     */
--- src/controller/CustomizeCakeController.java
+++ src/controller/CustomizeCakeController.java
@@ -1,6 +1,15 @@
 package controller;
 
+import view.*;
+import model.*;
+
 
 public class CustomizeCakeController {
-    
+	CustomizeCakeView cView;
+	
+	public CustomizeCakeController(CustomizeCakeView cView){
+		this.cView=cView;
+		
+	}
+		
 }

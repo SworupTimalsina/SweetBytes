d5bb4ab78542c5d5b68635ae0f0256baa140e904
Allan Gautam <allan.gtm92@gmail.com>
2023-07-02 21:37:34 +0545

    created model and controller for Items

---
+++ src/controller/ItemsController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import model.*;
+import view.*;
+
+
+public class ItemsController {
+	
+	ItemsModel iModel;
+	ItemsView iView;
+	
+	public ItemsController(ItemsView iView){
+		this.iView=iView;
+	}
+}

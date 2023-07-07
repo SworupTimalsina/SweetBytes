84fbeb48ca34e727b24817d8e7c686d310359405
Purnima8555 <110591365+Purnima8555@users.noreply.github.com>
2023-07-07 13:41:46 +0545

    working on review

---
+++ src/Database/DbConnection.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Database;
+
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author DELL
+ */
+public class DbConnection {
+    public static Connection dbconnect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn=DriverManager.getConnection("jdbx:mysql://localhost:3306/project_sb","root","Ranapurnima123$$");
+                    
+            System.out.println("connected");
+            return(conn);
+        }
+    
+    
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+
+    }
+}
